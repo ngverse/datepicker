111707a5e8b02de5908108f37974c7a53997ea16
luka onik <luka.onikadze@toptal.com>
2025-03-19 21:29:19 +0400

    chore: add datepicker library

---
+++ projects/datepicker/src/lib/datepicker.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-datepicker',
+  imports: [],
+  template: `
+    <p>
+      datepicker works!
+    </p>
+  `,
+  styles: ``
+})
+export class DatepickerComponent {
+
+}
---
+++ projects/datepicker/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of datepicker
+ */
+
+export * from './lib/datepicker.service';
+export * from './lib/datepicker.component';
